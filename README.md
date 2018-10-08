fd3cf5383757df7a76c2ac4d65a66fea63d3020f
bmkazuya <laughing00boomer@gmail.com>
2018-10-08 16:16:19 +0900

    wxPythonにmatplotlib埋め込み、README.md修正

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # Python training
 
+## Matplotlib
+### `plt.figure()`, `fig.add_subplot()`
+```py
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+axs = fig.add_subplot(111)
+line, = axs.plot([], [])
+line.set_data([1, 2, 3], [2, 3, 1])
+fig.show()
+```
+
+### `plt.subplots()`
+```py
+import matplotlib.pyplot as plt
+
+fig, axs = plt.subplots(2, 2)
+line.append(axs[0, 0].plot([], [])
+line[0].set_data([1, 2, 3], [2, 3, 1])
+fig.show()
+```
+
+### wxPythonに埋め込み
+```py
+import matplotlib
+matplotlib.interactive(True)
+matplotlib.use('WXAgg')
+
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
+from matplotlib.figure import Figure
+
+import wx
+
+app = wx.App()
+frame = wx.Frame(None, wx.ID_ANY, 'Test Frame')
+panel = wx.Panel(frame, wx.ID_ANY, pos=(0, 0))
+layout = wx.BoxSizer(wx.VERTICAL)
+
+fig = plt.figure()
+axs = fig.add_subplot(111)
+line, = axs.plot([], [])
+
+canvas = FigureCanvasWxAgg(panel, wx.ID_ANY, fig)
+layout.Add(canvas, flag=wx.GROW)
+panel.SetSizer(layout)
+
+line.set_data([1, 2, 3], [2, 3, 1])
+canvas.draw()
+
+frame.Show()
+app.MainLoop()
+```
+
+## wxPython
+
+- [wxPython | Python-izm](https://www.python-izm.com/gui/wxpython/)
+
+```py
+import wx
+
+app = wx.App()
+frame = wx.Frame(None, wx.ID_ANY, 'Test Frame')
+panel = wx.Panel(frame, wx.ID_ANY, pos=(0, 0))
+
+stext = wx.StaticText(panel, wx.ID_ANY, 'Static Text')
+
+layout = wx.BoxSizer(wx.VERTICAL)
+layout.Add(stext)
+
+panel.SetSizer(layout)
+frame.Show()
+app.MainLoop()
+```
+
+
--- app4/example02.py
+++ app4/example02.py
@@ -1,9 +1,11 @@
 import wx
 
 import matplotlib
-matplotlib.use('WX')
+matplotlib.interactive(True)
+matplotlib.use('WXAgg')
 
-import matplotlib.pyplot as plt
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
+from matplotlib.figure import Figure
 
 '''
 変調度 [%]
@@ -14,7 +16,8 @@ import matplotlib.pyplot as plt
 def slider_value_change(event):
     obj = event.GetEventObject()
     frame.SetStatusText('Slider value change')
-    print(obj.GetValue())
+    line.set_data([1, 2, 3], [i + obj.GetValue() / 50.0 for i in [1, 2, 0]])
+    canvas.draw()
 
 application = wx.App()
  
@@ -43,15 +46,25 @@ slider_2 = wx.Slider(panel, style=wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_LAB
 slider_2.SetMin(-100)
 slider_2.SetMax(100)
 slider_2.SetValue(0)
-slider_2.Bind(wx.EVT_SLIDER, slider_value_change)
+# slider_2.Bind(wx.EVT_SLIDER, slider_value_change)
 # パイロット
 slider_3 = wx.Slider(panel, style=wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_LABELS)
 slider_3.SetMin(0)
 slider_3.SetMax(100)
 slider_3.SetValue(5)
-slider_3.Bind(wx.EVT_SLIDER, slider_value_change)
+# slider_3.Bind(wx.EVT_SLIDER, slider_value_change)
 
-layout_1 = wx.GridSizer(3, 2, 3, 2)
+# プロット領域作成
+fig = Figure()
+axs = fig.add_subplot(111)
+line, = axs.plot([], [])
+axs.set_xlim((1, 3))
+axs.set_ylim((0, 4))
+# fig.tight_layout() ## tight_layout : falling back to Agg renderer
+line.set_data([1, 2, 3], [2, 3, 1])
+canvas = FigureCanvasWxAgg(panel, wx.ID_ANY, fig)
+
+layout_1 = wx.GridSizer(4, 2, 4, 2)
 
 layout_1.Add(stext_1, flag=wx.GROW)
 layout_1.Add(slider_1, flag=wx.GROW)
@@ -59,6 +72,7 @@ layout_1.Add(stext_2, flag=wx.GROW)
 layout_1.Add(slider_2, flag=wx.GROW)
 layout_1.Add(stext_3, flag=wx.GROW)
 layout_1.Add(slider_3, flag=wx.GROW)
+layout_1.Add(canvas, flag=wx.GROW)
 
 panel.SetSizer(layout_1)
 
