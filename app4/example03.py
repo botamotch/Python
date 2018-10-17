eeaeda2e89ce05d9f17b1fe6253faee3a5fe5d3b
bmkazuya <laughing00boomer@gmail.com>
2018-10-17 21:42:18 +0900

    app4/example03.py ABC Simアプリ ver 1.0

--- app4/example03.py
+++ app4/example03.py
@@ -14,7 +14,7 @@ from matplotlib.figure import Figure
 
 class SimFrame(wx.Frame):
     def __init__(self):
-        super().__init__(None, wx.ID_ANY, 'テストフレーム', size=(1000, 800))
+        super().__init__(None, wx.ID_ANY, 'Mech-Zehnder Modulator Controller Simulator', size=(700, 800))
         self.CreateStatusBar()
         self.SetStatusText('python-izm.com')
         self.GetStatusBar().SetBackgroundColour(None)
@@ -23,18 +23,26 @@ class SimFrame(wx.Frame):
 
         root_panel = wx.Panel(self, wx.ID_ANY)
 
-        panel_1 = TitlePanel(root_panel)
-        panel_2 = InputPanel(root_panel)
-        panel_3 = OutputPanel(root_panel)
+        self.panel_1 = TitlePanel(root_panel)
+        self.panel_2 = InputPanel(root_panel)
+        self.panel_3 = OutputPanel(root_panel)
 
         root_layout = wx.BoxSizer(wx.VERTICAL)
-        root_layout.Add(panel_1, 0, wx.GROW | wx.ALL, border=10)
-        root_layout.Add(panel_2, 0, wx.GROW | wx.ALL, border=10)
-        root_layout.Add(panel_3, 0, wx.GROW | wx.ALL, border=10)
+        root_layout.Add(self.panel_1, 0, wx.GROW | wx.ALL, border=10)
+        root_layout.Add(self.panel_2, 0, wx.GROW | wx.ALL, border=10)
+        root_layout.Add(self.panel_3, 0, wx.GROW | wx.ALL, border=10)
         root_panel.SetSizer(root_layout)
         root_layout.Fit(root_panel)
 
-        panel_2.replot()
+        self.panel_1.slider_1.Bind(wx.EVT_SLIDER, self.replot)
+        self.panel_1.slider_2.Bind(wx.EVT_SLIDER, self.replot)
+        self.panel_1.slider_3.Bind(wx.EVT_SLIDER, self.replot)
+
+    def replot(self, event=wx.EVT_SLIDER):
+        mod_degree = self.panel_1.slider_1.GetValue() / 100.0
+        bias_point = self.panel_1.slider_2.GetValue() / 100.0
+
+        self.panel_2.replot(bias_point, mod_degree)
 
 # -----------------------------------------------------------------------------
 #  Menu bar
@@ -60,42 +68,48 @@ class TitlePanel(wx.Panel):
         super().__init__(parent, wx.ID_ANY)
         self.SetBackgroundColour('#FFFFEE')
 
+        font_1 = wx.Font(16, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
+        font_2 = wx.Font(12, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
+
         title_text = wx.StaticText(self, wx.ID_ANY, 'Mach-Zehnder Modulator Controller Simulator')
-        font = wx.Font(16, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
-        title_text.SetFont(font)
+        title_text.SetFont(font_1)
 
         panel_controller = wx.Panel(self, wx.ID_ANY)
 
-        stext_1 = wx.StaticText(panel_controller, wx.ID_ANY, 'Mod. degree [%]')
-        stext_2 = wx.StaticText(panel_controller, wx.ID_ANY, 'Bias point [%]')
-        stext_3 = wx.StaticText(panel_controller, wx.ID_ANY, 'Pilot [%]')
-        slider_1 = wx.Slider(panel_controller, style=wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_LABELS)
-        slider_1.SetMin(0)
-        slider_1.SetMax(100)
-        slider_1.SetValue(30)
-        slider_2 = wx.Slider(panel_controller, style=wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_LABELS)
-        slider_2.SetMin(-100)
-        slider_2.SetMax(100)
-        slider_2.SetValue(0)
-        slider_3 = wx.Slider(panel_controller, style=wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_LABELS)
-        slider_3.SetMin(0)
-        slider_3.SetMax(100)
-        slider_3.SetValue(5)
+        stext_1 = wx.StaticText(panel_controller, wx.ID_ANY, '\nMod. degree [%]')
+        stext_2 = wx.StaticText(panel_controller, wx.ID_ANY, '\nBias point [%]')
+        stext_3 = wx.StaticText(panel_controller, wx.ID_ANY, '\nPilot [%]')
+        stext_1.SetFont(font_2)
+        stext_2.SetFont(font_2)
+        stext_3.SetFont(font_2)
+
+        self.slider_1 = wx.Slider(panel_controller, style=wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_VALUE_LABEL)
+        self.slider_1.SetMin(0)
+        self.slider_1.SetMax(100)
+        self.slider_1.SetValue(30)
+        self.slider_2 = wx.Slider(panel_controller, style=wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_VALUE_LABEL)
+        self.slider_2.SetMin(-100)
+        self.slider_2.SetMax(100)
+        self.slider_2.SetValue(0)
+        self.slider_3 = wx.Slider(panel_controller, style=wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_VALUE_LABEL)
+        self.slider_3.SetMin(0)
+        self.slider_3.SetMax(100)
+        self.slider_3.SetValue(5)
 
         layout_controller = wx.FlexGridSizer(rows=3, cols=2, gap=(0, 0))
         layout_controller.Add(stext_1, flag=wx.GROW)
-        layout_controller.Add(slider_1, flag=wx.GROW)
+        layout_controller.Add(self.slider_1, flag=wx.GROW)
         layout_controller.Add(stext_2, flag=wx.GROW)
-        layout_controller.Add(slider_2, flag=wx.GROW)
+        layout_controller.Add(self.slider_2, flag=wx.GROW)
         layout_controller.Add(stext_3, flag=wx.GROW)
-        layout_controller.Add(slider_3, flag=wx.GROW)
+        layout_controller.Add(self.slider_3, flag=wx.GROW)
         layout_controller.AddGrowableCol(1)
 
         panel_controller.SetSizer(layout_controller)
 
         layout = wx.BoxSizer(wx.VERTICAL)
-        layout.Add(title_text, flag=wx.GROW)
-        layout.Add(panel_controller, flag=wx.GROW)
+        layout.Add(title_text, flag=wx.GROW | wx.ALL, border=10)
+        layout.Add(panel_controller, flag=wx.GROW | wx.ALL, border=10)
         self.SetSizer(layout)
 
 # -----------------------------------------------------------------------------
@@ -108,18 +122,13 @@ class InputPanel(wx.Panel):
 
         self.fig = Figure()
 
-        self.axs_1 = self.fig.add_subplot(1, 2, 1)
-        self.axs_2 = self.fig.add_subplot(1, 2, 2)
-        self.axs_1.set_xlim((-10, 10))
-        self.axs_1.set_ylim((0, 1))
-        self.axs_2.set_xlim((-np.pi, np.pi))
-        self.axs_2.set_ylim((0, 1.1))
-        # self.fig.tight_layout()
+        self.axs = self.fig.add_subplot(1, 1, 1)
+        self.axs.set_xlim((-np.pi, np.pi))
+        self.axs.set_ylim((0, 1.1))
 
-        self.line_1, = self.axs_1.plot([], [], color='black', linestyle='-', linewidth=1.0)
-        self.line_2, = self.axs_2.plot([], [], color='black', linestyle='--', linewidth=0.5)
-        self.line_3, = self.axs_2.plot([], [], color='blue', linestyle='-', linewidth=1.0)
-        self.line_4, = self.axs_2.plot([], [], color='blue', linestyle='None', marker='o')
+        self.line_2, = self.axs.plot([], [], color='black', linestyle='--', linewidth=0.5)
+        self.line_3, = self.axs.plot([], [], color='blue', linestyle='-', linewidth=2.0)
+        self.line_4, = self.axs.plot([], [], color='blue', linestyle='None', marker='o')
 
         self.canvas = FigureCanvasWxAgg(self, wx.ID_ANY, self.fig)
 
@@ -132,11 +141,12 @@ class InputPanel(wx.Panel):
         self.line_2.set_data(xp, yp)
 
     def replot(self, bias_point=0.0, mod_degree=0.3):
-        xp = np.arange(bias_point -np.pi * mod_degree, bias_point + np.pi * mod_degree, 0.01)
+        xp = np.arange(np.pi * (bias_point - mod_degree), np.pi * (bias_point + mod_degree), 0.01)
         yp = 0.5 - 0.5*np.cos(xp)
         self.line_3.set_data(xp, yp)
-        yp = 0.5 - 0.5*np.cos([bias_point -np.pi * mod_degree, bias_point + np.pi * mod_degree])
-        self.line_4.set_data([bias_point -np.pi * mod_degree, bias_point + np.pi * mod_degree], yp)
+        xp = np.array([np.pi * (bias_point - mod_degree), np.pi * (bias_point + mod_degree)])
+        yp = 0.5 - 0.5*np.cos(xp)
+        self.line_4.set_data(xp, yp)
         self.canvas.draw()
 
 # -----------------------------------------------------------------------------
@@ -152,4 +162,5 @@ if __name__ == '__main__':
     application = wx.App()
     frame = SimFrame()
     frame.Show()
+    frame.replot()
     application.MainLoop()
